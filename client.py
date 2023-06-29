0e5237f130f21a950f49f9227efae43c5d7e8049
dontsaynetwork <137604590+dontsaynetwork@users.noreply.github.com>
2023-06-29 22:48:42 +0400

    Update client.py

--- client.py
+++ client.py
@@ -1,23 +1,22 @@
-import socket
+import socket #importing the socket librairy to use in the script
 
-client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client = socket.socket(socket.AF_INET,socket.SOCK_STREAM) #initialising the socket by using TCP/IP
 
-server = "127.0.0.1"
+server = "127.0.0.1" #remote server goes here. 127.0.0.1 is used to test the script on the same computer as the client
 port = 4321
 
-client.connect((server,port))
+client.connect((server,port)) #starting the connection
 
 while True:
     print("REMOTE SHELL OPERATOR")
     print("Enter your commands")
-    inp = input("...")
+    inp = input("") #Requesting the command to be executed by the remote computer
 
-    client.send(inp.encode())
+    client.send(inp.encode()) #sending the command to the remote server and waiting for a response
 
-    response = client.recv(1024)
+    response = client.recv(1024) #receiving the response and saving it in the variable response
 
     print("COMMAND SENT: ", inp)
     print("REPONSE FROM SERVER")
-    print(response.decode())
+    print(response.decode()) #printing the response from the remote server.
 
-#client.close()
